eefed144c1bed897b8d3df3e87385668551632e5
connorcinna <connorcinna@gmail.com>
2021-03-07 15:55:50 -0500

    backup

--- clinicfinder.js
+++ clinicfinder.js
@@ -19,19 +19,33 @@ function error_location() {
     console.log('error finding location');
 }
 function setup_map(center) {
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11', 
-        center: center,
-        maxBounds: [[-180, -85], [180, 85]],
-        zoom: 15
+        map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11', 
+            center: center,
+            maxBounds: [[-180, -85], [180, 85]],
+            zoom: 15
     });
     const nav = new mapboxgl.NavigationControl();
     map.addControl(nav);
     const directions = new MapboxDirections({
         accessToken : mapboxgl.accessToken
     });
+
+    directions.setOrigin([center[0], center[1]]);
+    console.log('origin set to: ' + center[0] + ' ' + center[1]);
     map.addControl(directions, 'top-left');
+    map.on('load', function () {
+
+    window.setInterval(function () {
+        var destination_node = document.getElementById("mapboxgl-directions-destination-input");
+        var html_content = destination_node.innerHTML;
+        var text_content = destination_node.textContent;
+        if (typeof text_content != null) { console.log(text_content); }
+    }, 2000);
+    
+});
+
 }
 
 function show_map() {
@@ -39,8 +53,4 @@ function show_map() {
     map_div.style.display = "block";
 }
 
-
-
-//var url = 'the website url'
-//data will be retrieved from this URL to be used in the map function
 
